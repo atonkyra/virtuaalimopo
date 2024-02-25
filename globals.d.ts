3561e18b1393f9748d328fa03d8e01879aa00d5f
rubiesonthesky <2591240+rubiesonthesky@users.noreply.github.com>
2024-02-25 22:06:17 +0200

    Update code
    - Update d3 from version ancient to v7
    - Remove jquery
    - Add typings
    - Update JS to ES22
    - Format with Prettier default settings

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,26 @@
-declare var config: Record<string, any>;
 type D3 = import("@types/d3");
 declare var d3: D3;
+
+interface Config {
+  keul_factor: number;
+  mopo_x: number;
+  mopo_y: number;
+  event_datasource: string;
+  datasource: string;
+  seg_on: string;
+  seg_off: string;
+  bg_day: string;
+  bg_night: string;
+  max_rotate: number;
+}
+
+declare var config: Config;
+
+type XY = [x: number, y: number];
+type Points = [XY, XY, XY, XY, XY, XY];
+
+interface Data {
+  interface: string;
+  pps: number;
+  bps: number;
+}
--- js/loop.js
+++ js/loop.js
@@ -1,180 +1,187 @@
-//setInterval(onUpdate, 33);
-var iter = 0;
-var accum = 0;
-var mopo_x = config['mopo_x'];
-var mopo_y = config['mopo_y'];
+let iter = 0;
+const mopo_x = config["mopo_x"];
+const mopo_y = config["mopo_y"];
 
-var night = false;
+let night = false;
 
-d3.select("#mopodiv")
-    .append("svg")
-    .attr("id","mopotolppa_outer")
-    .attr("width", mopo_x)
-    .attr("height", "800px")
-    .attr("style", "margin-top: 440px; position: absolute; z-index: -1;");
+const mopoDiv = d3.select("#mopodiv");
+mopoDiv.attr(
+  "style",
+  `width: ${mopo_x}px; height: ${mopo_y}px; margin-left: auto; margin-right: auto;`
+);
+mopoDiv
+  .append("svg")
+  .attr("id", "mopotolppa_outer")
+  .attr("width", mopo_x)
+  .attr("height", "800px")
+  .attr("style", "margin-top: 400px; position: absolute; z-index: -1;");
+mopoDiv
+  .append("svg")
+  .attr("id", "mopo_outer")
+  .attr("width", mopo_x)
+  .attr("height", mopo_y);
+mopoDiv
+  .append("svg")
+  .attr("id", "lisakyltti_outer")
+  .attr("width", mopo_x)
+  .attr("style", "");
 
-d3.select("#mopodiv")
-    .attr("style", "width: "+mopo_x+"px; height: "+mopo_y+"px; margin-left: auto; margin-right: auto;")
-    .append("svg")
-    .attr("id","mopo_outer")
-    .attr("width", mopo_x)
-    .attr("height", mopo_y);
-d3.select("#mopodiv")
-    .append("svg")
-    .attr("id","lisakyltti_outer")
-    .attr("width", mopo_x)
-    .attr("style", "");
+d3.svg("svg/varoituskolmio.svg").then((svg) => {
+  const importedNode = document.importNode(svg.documentElement, true);
+  d3.select("#mopo_outer").insert(() => importedNode);
+  d3.select("#Layer_1").attr("width", mopo_x);
 
+  d3.svg("svg/mopo.svg").then((svg) => {
+    const importedNode = document.importNode(svg.documentElement, true);
+    d3.select("#mopo_outer").insert(() => importedNode);
 
-d3.xml("svg/varoituskolmio.svg", "image/svg+xml", function(xml) {
-    var importedNode = document.importNode(xml.documentElement, true);
-    d3.select("#mopo_outer").node().appendChild(importedNode);
-    d3.select("#Layer_1")
-        .attr("width", mopo_x);
+    d3.select("#mopo_svg").attr("width", mopo_x).attr("height", mopo_y);
 
-    d3.xml("svg/mopo.svg", "image/svg+xml", function(xml) {
-        var importedNode = document.importNode(xml.documentElement, true);
-        d3.select("#mopo_outer").node().appendChild(importedNode);
-
-        d3.select("#mopo_svg")
-            .attr("width", mopo_x)
-            .attr("height", mopo_y);
-
-        d3.select("#mopo")
-            .attr("transform", "translate(50,80) scale(0.5) rotate(0, "+cx+", "+cy+")")
-            .attr("fill", "#202020");
-        if (!night)
-        {
-            d3.select("#mopo_svg").selectAll("ellipse").attr("visibility", "hidden");
-        }
-    });
+    d3.select("#mopo")
+      .attr("transform", `translate(50,80) scale(0.5) rotate(0, ${cx}, ${cy})`)
+      .attr("fill", "#202020");
+    if (!night) {
+      d3.select("#mopo_svg").selectAll("ellipse").attr("visibility", "hidden");
+    }
+  });
 });
 
-d3.xml("svg/mopotolppa.svg", "image/svg+xml", function(xml) {
-    var importedNode = document.importNode(xml.documentElement, true);
-    d3.select("#mopotolppa_outer").node().appendChild(importedNode);
-    d3.select("#mopotolppa")
-        .attr("width", mopo_x);
-
+d3.svg("svg/mopotolppa.svg").then((svg) => {
+  const importedNode = document.importNode(svg.documentElement, true);
+  d3.select("#mopotolppa_outer").insert(() => importedNode);
+  d3.select("#mopotolppa").attr("width", mopo_x);
 });
 
-d3.xml("svg/lisakyltti.svg", "image/svg+xml", function(xml) {
-        var importedNode = document.importNode(xml.documentElement, true);
-        d3.select("#lisakyltti_outer").node().appendChild(importedNode);
-        d3.select("#lisakyltti")
-            .attr("width", mopo_x);     
+d3.svg("svg/lisakyltti.svg").then((svg) => {
+  const importedNode = document.importNode(svg.documentElement, true);
+  d3.select("#lisakyltti_outer").insert(() => importedNode);
+  d3.select("#lisakyltti").attr("width", mopo_x);
 });
 
-var cx=162.052,cy=136.745,cs=0.5;
+const cx = 162.052;
+const cy = 136.745;
+const cs = 0.5;
 
-var avg_arr = new Array();
-var num_samples = 100;
-for(i=0;i<num_samples;i++) {
-    avg_arr.push(0);
+const avg_arr = new Array();
+const num_samples = 1000;
+for (let i = 0; i < num_samples; i++) {
+  avg_arr.push(0);
 }
 
-
-function setBackgroundColor(day)
-{
-    var t = new Date();
-    if ((t.getHours() >= 16 || t.getHours() <= 8) && day === undefined)
-    {
-        document.body.style.background = "#101010";
-        d3.select("#mopo").attr("fill", "#202020");
-        d3.select("#mopo_svg").selectAll("ellipse").attr("visibility", "visible");
-        night = true;
-    }
-    else {
-        document.body.style.background = "#f0f0f0";
-        d3.select("#mopo").attr("fill", "#000000");
-        d3.select("#mopo_svg").selectAll("ellipse").attr("visibility", "hidden");
-        night = false;
-    }
+function setBackgroundColor() {
+  const hours = new Date().getHours();
+  night = hours >= 16 || hours <= 8;
+  if (night) {
+    document.body.style.background = config["bg_night"];
+    d3.select("#mopo").attr("fill", "#202020");
+    d3.select("#mopo_svg").selectAll("ellipse").attr("visibility", "visible");
+  } else {
+    document.body.style.background = config["bg_day"];
+    d3.select("#mopo").attr("fill", "#000000");
+    d3.select("#mopo_svg").selectAll("ellipse").attr("visibility", "hidden");
+  }
 }
 
-var source = null;
+let source = null;
 function establishEventChannel() {
-    if(source !== null) { source.close(); }
-    source = new EventSource(config['event_datasource']);
-    source.addEventListener('message', function(e) {
-        edata = JSON.parse(e.data);
-        mbps = edata.bps/1000.0/1000.0;
-        traffic = parseInt(post_return_avg(parseInt(mbps)));
-        numberTo7Seg(traffic);
-        var d3m = d3.select("#mopo_svg")
-            .attr("transform", "translate(50,80) scale(0.5) rotate("+traffic*config['keul_factor']+", "+cx+", "+cy+")");
-    }, false);
-    source.addEventListener('error', function(e) {
-        establishEventChannel();
-    }, false);
+  if (source !== null) {
+    source.close();
+  }
+  source = new EventSource(config["event_datasource"]);
+  source.addEventListener(
+    "message",
+    (e) => {
+      /** @type {Data} */
+      const edata = JSON.parse(e.data);
+      const mbps = +edata.bps / 1000 / 1000;
+      updateMopo(mbps);
+    },
+    false
+  );
+  source.addEventListener(
+    "error",
+    () => {
+      establishEventChannel();
+    },
+    false
+  );
 }
 
-if(!!window.EventSource && config['event_datasource'] != '') {
-    establishEventChannel();
-    requestAnimationFrame(eventUpdate);
+if (!!window.EventSource && config["event_datasource"] != "") {
+  establishEventChannel();
+  requestAnimationFrame(eventUpdate);
 } else {
-    requestAnimationFrame(xhrUpdate);
+  requestAnimationFrame(xhrUpdate);
 }
 
 function xhrUpdate() {
-    var traffic = 0;
-    if(iter % 10 == 0) {
-        $.get(config['datasource'], function(traffic_value) {
-            traffic = parseInt(post_return_avg(parseInt(traffic_value)));
-            numberTo7Seg(traffic);
-            var d3m = d3.select("#mopo_svg").selectAll("path")
-                .attr("transform", "translate(50,80) scale(0.5) rotate("+traffic*config['keul_factor']+", "+cx+", "+cy+")");
-        });
-    } else {
-        traffic = post_return_avg(parseInt(avg_arr[num_samples-1]));
-        numberTo7Seg(traffic);
-        var d3m = d3.select("#mopo_svg").selectAll("path")
-            .attr("transform", "translate(50,80) scale(0.5) rotate("+traffic*config['keul_factor']+", "+cx+", "+cy+")");
-        var d3m = d3.select("#mopo_svg").selectAll("ellipse")
-            .attr("transform", "translate(50,80) scale(0.5) rotate("+traffic*config['keul_factor']+", "+cx+", "+cy+")");
-    }
+  if (iter % 100 == 0) {
+    fetch(config["datasource"])
+      .then((response) => response.json())
+      .then((edata) => {
+        const mbps = edata.bps / 1000 / 1000;
+        updateMopo(mbps);
+      });
+  } else {
+    updateMopo(avg_arr[num_samples - 1]);
+  }
 
-    ++iter;
-    requestAnimationFrame(xhrUpdate);
+  ++iter;
+  requestAnimationFrame(xhrUpdate);
 }
 
 function eventUpdate() {
-    var traffic = 0;
-    traffic = post_return_avg(parseInt(avg_arr[num_samples-1]));
-    numberTo7Seg(traffic);
-    var d3m = d3.select("#mopo_svg").selectAll("path")
-        .attr("transform", "translate(50,80) scale(0.5) rotate("+traffic*config['keul_factor']+", "+cx+", "+cy+")");
-    var d3m = d3.select("#mopo_svg").selectAll("ellipse")
-        .attr("transform", "translate(50,80) scale(0.5) rotate("+traffic*config['keul_factor']+", "+cx+", "+cy+")")
+  updateMopo(avg_arr[num_samples - 1]);
 
-    ++iter;
-    requestAnimationFrame(eventUpdate);
+  ++iter;
+  requestAnimationFrame(eventUpdate);
 }
 
+/**
+ * @param {number} mbps
+ */
+function updateMopo(mbps) {
+  const traffic = post_return_avg(mbps);
+  numberTo7Seg(traffic);
+
+  const maxRotate = config["max_rotate"];
+  const rawRotate = traffic * config["keul_factor"];
+  const rotate = maxRotate ? Math.min(rawRotate, maxRotate) : rawRotate;
+  const translateX = maxRotate ? 50 - (4 * rotate) / 9 : 50;
+  const transform = `translate(${translateX},80) scale(0.5) rotate(${rotate}, ${cx}, ${cy})`;
+  const mopoSvg = d3.select("#mopo_svg");
+  mopoSvg.selectAll("path").attr("transform", transform);
+  mopoSvg.selectAll("ellipse").attr("transform", transform);
+}
+
+/**
+ * @param {number} traffic_value
+ */
 function post_return_avg(traffic_value) {
-    avg_arr.shift();
-    avg_arr.push(traffic_value);
-    var avg = 0;
-    for(i=0;i<num_samples;i++) {
-        avg += avg_arr[i];
-    }
-
-    avg = avg / num_samples;
-
-    return Math.round(avg);
+  avg_arr.shift();
+  avg_arr.push(Math.round(traffic_value));
+  let sum = 0;
+  for (let i = 0; i < num_samples; i++) {
+    sum += avg_arr[i];
+  }
+  return Math.round(sum / num_samples);
 }
 
+/**
+ * @param {number} number
+ */
 function numberTo7Seg(number) {
-    var max = 5;
-    for(i=1;i<=max;i++) {
-        base = Math.pow(10,i);
-        segnum = number % base;
-        number -= segnum;
-        segnum = segnum / (base/10);
-        numbers[max-i].data(to7seg(segnum.toString())).style({"fill": function (d) {
-            return d ? config['seg_on'] : config['seg_off'];
-        }});
-    }
+  let max = 5;
+  // Make sure there is exactly 5 digits and take only 5 last
+  const parts = number.toString().padStart(max, "0").split("").slice(-max);
+  for (let i = 0; i < 5; i++) {
+    const element = numbers[i];
+    element
+      .selectAll("polygon")
+      .data(to7seg(parts[i]))
+      .join("polygon")
+      .attr("fill", (d) => (d ? config["seg_on"] : config["seg_off"]));
+  }
 }
 
 setInterval(setBackgroundColor, 60000);
